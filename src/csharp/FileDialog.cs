eef5a16f0108fa77194b2a6208da70e21d035ede
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2019-02-24 18:27:21 +1100

    Updated to new godot version, fixed file dialog reverting to old version and added nhunspell dictionaries

--- src/csharp/FileDialog.cs
+++ src/csharp/FileDialog.cs
@@ -1,28 +1,27 @@
 using Godot;
 using System;
-using System.Windows;
 using System.Windows.Forms;
+using NHunspell;
+
 
 public class FileDialog : Node
 {
 
 	public override void _Ready()
 	{
-
+		
 	}
 
 	[STAThread]
 	public static string OpenFileDialog()
 	{
-		// Try catch blocks seem to reduce the likelihood of crashing.
-		try
-		{
+		
 			var openFileDialog = new OpenFileDialog
 			{
 				InitialDirectory = "",
 				Filter = "Poopliga files (*.poopliga, *.ppga, *.json)|*.poopliga;*.ppga;*.json|All files (*.*)|*.*",
 				FilterIndex = 1,
-				RestoreDirectory = false
+				RestoreDirectory = true
 			};
 
 			var result = openFileDialog.ShowDialog();
@@ -31,41 +30,32 @@ public class FileDialog : Node
 				var path = openFileDialog.FileName;
 				return path;
 			}
-		}
-		catch
-		{
-			return null;
-		}
 
+
+		
 		return null;
 	}
 
 	[STAThread]
 	public static string SaveFileDialog()
 	{
-		// Try catch blocks seem to reduce the likelihood of crashing.
-		try
+		var saveFileDialog = new SaveFileDialog
 		{
-			var saveFileDialog = new SaveFileDialog
-			{
-				InitialDirectory = "",
-				Filter = "Poopliga files (*.poopliga, *.ppga, *.json)|*.poopliga;*.ppga;*.json|All files (*.*)|*.*",
-				FilterIndex = 1,
-				RestoreDirectory = false
-			};
+			InitialDirectory = "",
+			Filter = "Poopliga files (*.poopliga, *.ppga, *.json)|*.poopliga;*.ppga;*.json|All files (*.*)|*.*",
+			FilterIndex = 1,
+			RestoreDirectory = true
+		};
 
-			var result = saveFileDialog.ShowDialog();
-			if (result == DialogResult.OK)
-			{
-				var path = saveFileDialog.FileName;
-				return path;
-			}
-		}
-		catch
+		var result = saveFileDialog.ShowDialog();
+		if (result == DialogResult.OK)
 		{
-			return null;
+			var path = saveFileDialog.FileName;
+			return path;
 		}
 
+
+		
 		return null;
 	}
 
